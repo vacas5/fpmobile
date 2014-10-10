316aa1460e0359d39e3f6b758feb57da52b6ddac
Ubuntu <ubuntu@ip-172-31-20-68.us-west-2.compute.internal>
2014-10-10 03:19:30 +0000

    scripts images html and css for consulting page

--- js/main.js
+++ js/main.js
@@ -12,12 +12,12 @@ popcorn.toggleMenu = function(e) {
     // set variable to heading text in case they click again
     self.oldHeading = $heading.text();
 
-    $content.fadeOut('slow', function() {
+    $content.fadeOut('fast', function() {
       $heading.text('MENU');
       $menu.fadeIn('fast').removeClass('hidden');
     });
   } else {
-    $menu.fadeOut('slow', function() {
+    $menu.fadeOut('fast', function() {
       $content.fadeIn('fast');
       $heading.text(self.oldHeading);
     });
@@ -31,6 +31,17 @@ popcorn.enterHome = function(e) {
   });
 }
 
+popcorn.toggleAccordion = function(e) {
+  var $target = $(e.currentTarget),
+  $description = $target.next('.method_description');
+
+  if ($description.is(':hidden')) {
+    $description.slideDown('fast', 'linear');
+  } else {
+    $description.slideUp('fast', 'linear');
+  }
+}
+
 // event bindings
 $('#toggleMenu').click(function(e) {
   e.preventDefault();
@@ -40,4 +51,9 @@ $('#toggleMenu').click(function(e) {
 $('#enterHome').click(function(e) {
   e.preventDefault();
   popcorn.enterHome(e);
-})
+});
+
+$('#consultingAccordion .slide > a').click(function(e) {
+  e.preventDefault();
+  popcorn.toggleAccordion(e);
+});
