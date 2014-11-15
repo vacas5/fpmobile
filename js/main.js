0f43965b8a81ffbf436a89dda6c16806774bf523
Ubuntu <ubuntu@ip-172-31-20-68.us-west-2.compute.internal>
2014-11-15 15:56:42 +0000

    added scroll indicator

--- js/main.js
+++ js/main.js
@@ -2,18 +2,59 @@
 
 var popcorn = popcorn || {};
 
-popcorn.toggleMenu = function(e) {
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+popcorn.debounce = function (func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+popcorn.scrollHint = function() {
+  var heights = {
+    doc: {
+      height: $(document).height()
+    },
+    window: {
+      height: $(window).height(),
+      scroll: $(window).scrollTop()
+    }
+  }
+
+  if (heights.doc.height > heights.window.height) {
+    if (heights.window.scroll == 0) {
+      $('.scroll_hint.down').fadeIn('fast');
+    } else if ( (heights.window.scroll + heights.window.height) == heights.doc.height ) {
+      $('.scroll_hint.up').fadeIn('fast');
+    } else {
+      $('.scroll_hint').fadeIn('fast');
+    }
+  }
+};
+
+popcorn.toggleMenu = function() {
   var self = this,
-  $target = $(e.currentTarget);
-  $menu = $('#mainNav'),
-  $content = $('#mainContent'),
-  $heading = $('#mainHeading');
+    $menu = $('#mainNav'),
+    $content = $('#mainContent'),
+    $heading = $('#mainHeading');
 
   if ($menu.is(':hidden')) {
     // set variable to heading text in case they click again
     self.oldHeading = $heading.text();
 
     $content.fadeOut('fast', function() {
+      $('.scroll_hint').hide();
       $heading.text('MENU');
       $menu.fadeIn('fast').removeClass('hidden');
     });
@@ -23,14 +64,17 @@ popcorn.toggleMenu = function(e) {
       $heading.text(self.oldHeading);
     });
   }
-}
+};
 
-popcorn.enterHome = function(e) {
-  $('#enterHome, #fullSiteLink').fadeOut('slow', function() {
+popcorn.enterHome = function() {
+  var self = this;
+  $('#homeIntro').fadeOut('slow', function() {
     $('#mainContent').removeClass('home_intro');
-    $('#carousel').fadeIn('fast').removeClass('hidden');
+    $('#carousel').fadeIn('fast', function() {
+      self.scrollHint();
+    }).removeClass('hidden');
   });
-}
+};
 
 popcorn.toggleAccordion = function(e) {
   var $target = $(e.currentTarget),
@@ -55,13 +99,14 @@ popcorn.showBios = function(e) {
       scrollTop: toScroll - 40
     }, 300);
   });
-}
+};
 
-popcorn.showPress = function(e) {
+popcorn.showPress = function() {
+  var self = this;
   $('#pressHero').fadeOut('fast', function() {
     $('#pressMedia').fadeIn('fast');
-  })
-}
+  });
+};
 
 popcorn.createArticles = function(xml) {
   var self = this;
@@ -72,19 +117,21 @@ popcorn.createArticles = function(xml) {
       image: $(this).children().eq(7).attr('url'),
       date: $(this).find('pubDate').text(),
       description: $(this).find('description').text()
-    }
+    };
     markup.dateArray = markup.date.split(' ');
 
-    var slide = '<div class="article_wrapper">'
-    + '<a href="#" class="article_image" style="background-image:url(' + markup.image + ');" data-article="' + i + '"></a>'
-    + '<h2 class="article_title">' + markup.title + '</h2>'
-    + '<h3 class="article_date">' + markup.dateArray[2] + ' ' + markup.dateArray[1] + ', ' + markup.dateArray[3] + '</h3>'
-    + '<p class="article_description">' + markup.description + '</p>'
-    + '</div>';
+    var slide = '<div class="article_wrapper">' +
+    '<a href="#" class="article_image" style="background-image:url(' + markup.image + ');" data-article="' + i + '"></a>' +
+    '<h2 class="article_title">' + markup.title + '</h2>' +
+    '<h3 class="article_date">' + markup.dateArray[2] + ' ' + markup.dateArray[1] + ', ' + markup.dateArray[3] + '</h3>' +
+    '<p class="article_description">' + markup.description + '</p>' +
+    '</div>';
 
     $('#cultureArticleFeed').append(slide);
-  })
-}
+  });
+
+  popcorn.scrollHint();
+};
 
 popcorn.showArticle = function(e) {
   var self = this,
@@ -97,14 +144,14 @@ popcorn.showArticle = function(e) {
   link = $article.find('link').text(),
   uriTitle = encodeURIComponent(title);
 
-  var articleMarkup = '<a href="#" class="article_close">&times;</a>'
-  + '<div class="article_image"><img src="' + image + '"></div>'
-  + '<div class="article_social">'
-  + '<a target="_blank" href="http://www.facebook.com/sharer/sharer.php?u=' + link + '" class="facebook"></a>'
-  + '<a target="_blank" href="http://twitter.com/home?status=' + uriTitle + '%20' + link + '" class="twitter"></a>'
-  + '</div>'
-  + '<h2 class="article_title">' + title + '</h2>'
-  + '<div class="article_content">' + content + '</div>';
+  var articleMarkup = '<a href="#" class="article_close">&times;</a>' +
+  '<div class="article_image"><img src="' + image + '"></div>' +
+  '<div class="article_social">' +
+  '<a target="_blank" href="http://www.facebook.com/sharer/sharer.php?u=' + link + '" class="facebook"></a>' +
+  '<a target="_blank" href="http://twitter.com/home?status=' + uriTitle + '%20' + link + '" class="twitter"></a>' +
+  '</div>' +
+  '<h2 class="article_title">' + title + '</h2>' +
+  '<div class="article_content">' + content + '</div>';
 
   $('#cultureArticleWrapper').html(articleMarkup);
 
@@ -114,13 +161,13 @@ popcorn.showArticle = function(e) {
       scrollTop: 0
     }, 300);
   });
-}
+};
 
 popcorn.closeArticle = function() {
   $('#cultureArticleWrapper').fadeOut('fast', function() {
     $('#cultureArticleFeed').fadeIn('fast');
   });
-}
+};
 
 // event bindings
 $('#toggleMenu').click(function(e) {
@@ -156,4 +203,15 @@ $('body').on('click', '.article_wrapper > .article_image', function(e) {
 $('body').on('click', '.article_close', function(e) {
   e.preventDefault();
   popcorn.closeArticle();
+});
+
+$(window).scroll(popcorn.debounce(popcorn.scrollHint, 500));
+
+$(window).scroll(popcorn.debounce(function() {
+  $('.scroll_hint').hide();
+}, 500, true));
+
+$(document).ready(function() {
+  $('.scroll_hint').hide();
+  popcorn.scrollHint();
 });
